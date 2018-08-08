4165250ff0bb69b2c8b78d3a72c1a4347542b447
phil-6 <github@elasticparsley.uk>
2018-08-09 00:18:34 +0100

    write spots controller

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,43 @@
 class SpotsController < ApplicationController
+  before_action :set_spot, only: [:show, :update, :destroy]
+
+  # GET /spots
+  def index
+    @spots = Spot.all
+    json_response(@spots)
+  end
+
+  # POST /spots
+  def create
+    @spot = Spot.create!(spot_params)
+    json_response(@spot, :created)
+  end
+
+  # GET /spots/:id
+  def show
+    json_response(@spot)
+  end
+
+  # PUT /spots/:id
+  def update
+    @spot.update(spot_params)
+    head :no_content
+  end
+
+  # DELETE /spots/:id
+  def destroy
+    @spot.destroy
+    head :no_content
+  end
+
+  private
+
+  def spot_params
+    # whitelist params
+    params.permit(:name, :created_by, :description, :latitude, :longitude)
+  end
+
+  def set_spot
+    @spot = Spot.find(params[:id])
+  end
 end
