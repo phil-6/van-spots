fe63f28c69872c2d472494942364fb8d380df7a4
phil-6 <github@elasticparsley.uk>
2018-08-08 21:32:08 +0100

    update spot migration

--- db/migrate/002_create_spots.rb
+++ db/migrate/002_create_spots.rb
@@ -1,8 +1,11 @@
 class CreateSpots < ActiveRecord::Migration[5.2]
   def change
     create_table :spots do |t|
-      t.string :title
+      t.string :name
       t.string :created_by
+      t.text :description
+      t.decimal :latitude, precision: 10, scale: 6
+      t.decimal :longitude, precision: 10, scale: 6
 
       t.timestamps
     end
