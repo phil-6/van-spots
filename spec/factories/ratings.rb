c8d92a5614eafc5773cbebf32a852a8403a673e8
phil-6 <github@elasticparsley.uk>
2018-08-09 00:05:57 +0100

    small refactor

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -2,10 +2,10 @@
 FactoryBot.define do
   factory :rating do
     score { Faker::Number.between(0, 100) }
-
+    created_by { Faker::Number.number(10) }
     review_title { Faker::Food.dish }
     review_body { Faker::Movie.quote }
-    created_by { Faker::Number.number(10) }
+
     spot_id 1
   end
 end
