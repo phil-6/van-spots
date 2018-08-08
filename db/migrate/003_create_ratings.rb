1f6de69ee1536c30d485149a403269846c443a67
phil-6 <github@elasticparsley.uk>
2018-08-09 00:25:13 +0100

    add created_by to ratings migration

--- db/migrate/003_create_ratings.rb
+++ db/migrate/003_create_ratings.rb
@@ -2,6 +2,7 @@ class CreateRatings < ActiveRecord::Migration[5.2]
   def change
     create_table :ratings do |t|
       t.integer :score
+      t.string :created_by
       t.string :review_title
       t.text :review_body
       t.references :spot, foreign_key: true
