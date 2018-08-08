1c84469232db9cf48976854a2defb2254e149f2f
phil-6 <github@elasticparsley.uk>
2018-08-09 00:11:14 +0100

    uncomment important line

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
 Shoulda::Matchers.configure do |config|
