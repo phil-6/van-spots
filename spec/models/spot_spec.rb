29e59df5635559cb50d0d072d182642415680d35
phil-6 <github@elasticparsley.uk>
2018-08-08 22:50:39 +0100

    update spot_spec

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure Spot model has a 1:m relationship with the Rating model
+  it { should have_many(:ratings).dependent(:destroy) }
+
+  # Validation tests
+  # ensure columns are present before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:latitude) }
+  it { should validate_presence_of(:longitude) }
 end
