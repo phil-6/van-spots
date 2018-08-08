0823ff658e5e11a8d36b245edddbe52409678561
phil-6 <github@elasticparsley.uk>
2018-08-08 22:58:18 +0100

    add title and body to ratings migration

--- db/migrate/003_create_ratings.rb
+++ db/migrate/003_create_ratings.rb
@@ -2,7 +2,8 @@ class CreateRatings < ActiveRecord::Migration[5.2]
   def change
     create_table :ratings do |t|
       t.integer :score
-      t.string :review
+      t.string :review_title
+      t.text :review_body
       t.references :spot, foreign_key: true
 
       t.timestamps
