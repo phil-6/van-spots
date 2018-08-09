7cfbf8dc53b22b68d706127b946fcd863cc027a2
phil-6 <github@elasticparsley.uk>
2018-08-09 20:07:01 +0100

    get user model spec to pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_secure_password
 
   # model associations
-  has_many :spots, foreign_key: :user_id
-  has_many :ratings, dependent: :destroy, foreign_key: :user_id
+  has_many :spots, foreign_key: :created_by
+  has_many :ratings, dependent: :destroy, foreign_key: :created_by
 
   # validations
   validates_presence_of :username,
