08ba5058db573f0663ade9938ab1e72562021e11
phil-6 <github@elasticparsley.uk>
2018-08-09 19:29:22 +0100

    create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+# app/models/user
 class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
+
+  # model associations
+  has_many :spots, foreign_key: :user_id
+  has_many :ratings, dependent: :destroy, foreign_key: :user_id
+
+  # validations
+  validates_presence_of :username,
+                        :email,
+                        :description,
+                        :password_digest
 end
