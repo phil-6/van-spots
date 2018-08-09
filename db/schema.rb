ab1427620f38937ebd190ef9c756c32fd8c4e175
phil-6 <github@elasticparsley.uk>
2018-08-09 19:31:02 +0100

    refactor user.name to user.username

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.text "description"
     t.string "password_digest"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe User, type: :model do
 
   # Validation tests
   # ensure name, email and password_digest are present before save
-  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:username) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:password_digest) }
