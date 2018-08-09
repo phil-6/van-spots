eb3bd365fab172b2deb8e027897d720c5fd9993f
phil-6 <github@elasticparsley.uk>
2018-08-09 23:02:05 +0100

    add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seed 50 records
+50.times do
+  spot = Spot.create(
+      name: Faker::LordOfTheRings.location,
+      created_by: Faker::Number.number(10),
+      description: Faker::HitchhikersGuideToTheGalaxy.quote,
+      latitude: Faker::Number.decimal(4, 6),
+      longitude:  Faker::Number.decimal(4, 6))
+  spot.ratings.create(
+      score: Faker::Number.between(0, 100),
+      created_by: Faker::Number.number(10),
+      review_title: Faker::Food.dish,
+      review_body: Faker::Movie.quote,
+      spot_id: Faker::Number.between(1, 50) )
+end
