29d377561856d4409fe6457ec044dd98db1966e0
phil-veeqo <phil@veeqo.com>
2018-09-11 16:32:20 +0100

    create forms for create spot, update spot and create rating and update controller to render pages

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -13,16 +13,34 @@ class SpotsController < ApplicationController
     @spot
   end
 
+  # GET /spots/new
+  def new
+    @spot = Spot.new
+  end
+
   # POST /spots
   def create
     @spot = current_user.spots.create!(spot_params)
     #TODO Add Response
   end
 
+  # GET /spots/:id/edit
+  def edit
+    @spot = Spot.find(params[:id])
+  end
+
   # PUT /spots/:id
   def update
-    @spot.update(spot_params)
+    # @spot.update(spot_params)
     #TODO Add Response
+
+    @spot = Spot.find(params[:id])
+
+    if @spot.update(spot_params)
+      redirect_to @spot
+    else
+      render 'edit'
+    end
   end
 
   # DELETE /spots/:id
