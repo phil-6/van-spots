1c89f792406edbb1563c6203335cf8722e9a5c69
phil-6 <github@elasticparsley.uk>
2018-09-22 16:48:40 +0100

    update about and how-to to main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,21 @@
 class PagesController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: [:main, :map, :about, :how_to]
+  skip_before_action :authenticate_user!, only: [:main, :map]
   def main
     render template: "pages/main"
   end
 
   def about
-    render template: "pages/about"
+    render template: "pages/_about"
   end
 
-  def how_to
-    render template: "pages/how_to"
-  end
-
-  def map
-    render template: "pages/map"
-  end
+  # def how_to
+  #   render template: "pages/_how_to"
+  # end
+  #
+  # def map
+  #   render template: "pages/map"
+  # end
 
 
 end
