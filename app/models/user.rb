20d8f80bc5de32851ba50ad016171b94f766f71a
phil-veeqo <phil@veeqo.com>
2018-10-03 12:14:40 +0100

    add soft delete for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,21 @@ class User < ApplicationRecord
   has_many :spots, foreign_key: :user_id
   has_many :ratings, foreign_key: :user_id
 
+  # instead of deleting, indicate the user requested a delete & timestamp it
+  def soft_delete
+    update_attribute(:deleted_at, Time.current)
+  end
+
+  # ensure user account is active
+  def active_for_authentication?
+    super && !deleted_at
+  end
+
+  # provide a custom message for a deleted account
+  def inactive_message
+    !deleted_at ? super : :deleted_account
+  end
+
   # validations
   validates_presence_of :username,
                         :email,
