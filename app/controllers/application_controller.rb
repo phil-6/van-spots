73b63f469ef3dc631ab80613abf32e989e7c2521
phil-veeqo <phil@veeqo.com>
2018-10-04 10:21:42 +0100

    update application controller comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  # before any blog action happens, it will authenticate the user
+  # before any action happens, it will authenticate the user
   before_action :authenticate_user!
 
 end
