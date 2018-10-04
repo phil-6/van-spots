f412c401dc5eb8a3c749b577983e15a2a908c7e7
phil-veeqo <phil@veeqo.com>
2018-10-04 19:48:39 +0100

    get basic filter working on spots index

---
+++ app/assets/javascripts/card-filter.js
@@ -0,0 +1,49 @@
+function filterSelection(spot_type) {
+    var cards, i;
+    cards = document.getElementsByClassName("card");
+    if (spot_type === "all") spot_type = "";
+    // Add the "show" class to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < cards.length; i++) {
+        removeClass(cards[i], "show");
+        if (cards[i].className.indexOf(spot_type) > -1) addClass(cards[i], "show");
+    }
+}
+
+// Show filtered elements
+function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) === -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("filterButtons");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+
+
