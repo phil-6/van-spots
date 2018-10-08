65045cfc59f28117f6777e7b0ca5350ec6ccd06f
phil-veeqo <phil@veeqo.com>
2018-10-08 11:16:48 +0100

    add filter by rating buttons

--- app/assets/javascripts/card-filter.js
+++ app/assets/javascripts/card-filter.js
@@ -1,4 +1,4 @@
-function filterSelection(spot_type) {
+function filterByType(spot_type) {
     var cards, i;
     cards = document.getElementsByClassName("card");
     if (spot_type === "all") spot_type = "";
@@ -7,6 +7,21 @@ function filterSelection(spot_type) {
         removeClass(cards[i], "show");
         if (cards[i].className.indexOf(spot_type) > -1) addClass(cards[i], "show");
     }
+    changeActiveButton("spot_type");
+}
+
+function filterByRating(spot_rating) {
+    var cards, i;
+    cards = document.getElementsByClassName("card");
+    if (spot_rating === "all") spot_rating = "";
+    // Add the "show" class to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < cards.length; i++) {
+        removeClass(cards[i], "show");
+        for (s = spot_rating; s<=10; s++) {
+            if (cards[i].className.indexOf("rating-" + s) > -1) addClass(cards[i], "show");
+        }
+    }
+    changeActiveButton("rating");
 }
 
 // Show filtered elements
@@ -34,16 +49,27 @@ function removeClass(element, name) {
     element.className = arr1.join(" ");
 }
 
+function changeActiveButton(button_type) {
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("filterButtons");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
+    var btnContainer;
+    if (button_type === "spot_type"){
+        btnContainer = document.getElementById("type-filters");
+    }else if (button_type === "rating"){
+        btnContainer = document.getElementById("rating-filters");
+    }else {
+        console.log("button container type not known");
+    }
+
+    var btns = btnContainer.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = btnContainer.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
 }
+
 
 
 
