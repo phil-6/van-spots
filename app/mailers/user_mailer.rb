c078ad1ca4c1827840360267e4ee022cd8f1f15f
phil-6 <github@purpleriver.dev>
2019-09-16 11:15:38 +0100

    change email from ep to pr

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,6 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url  = 'https://vanspots.uk/users/sign_in'
-    mail(to: @user.email,from: 'welcome@vanspots.uk', bcc:'vanspotsnewuser@elasticparsley.uk', subject: 'Welcome to VanSpots')
+    mail(to: @user.email,from: 'welcome@vanspots.uk', bcc:'vanspotsnewuser@purpleriver.dev', subject: 'Welcome to VanSpots')
   end
 end
