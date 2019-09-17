9b9acda237622e0567d7c4b7fd570569c63e9166
phil-6 <github@purpleriver.dev>
2019-09-17 10:34:02 +0100

    update map to remove API key

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,13 @@ module VanSpots
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'app_environment_variables.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
+
