04480c7eefdd69bdfe7124f10ee524e25d9e378b
phil-6 <github@purpleriver.dev>
2019-11-06 14:26:51 +0000

    sort spots index by rating

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -8,7 +8,7 @@ class SpotsController < ApplicationController
     if params[:spot_type]
       @spots = Spot.where(:spot_type => params[:spot_type])
     else
-      @spots = Spot.all.includes(:user, :ratings)
+      @spots = Spot.all.includes(:user, :ratings).order('average_rating DESC')
     end
   end
 
